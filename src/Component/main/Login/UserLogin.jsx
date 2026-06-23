14cd1c6b68204319944abe7f337fa73c321e8809
Vishal Kumar <calvk3@gmail.com>
2026-06-24 00:10:04 +0530

    Update with backend

--- src/Component/main/Login/UserLogin.jsx
+++ src/Component/main/Login/UserLogin.jsx
@@ -9,7 +9,7 @@ const UserLogin = () => {
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:8080/api/auth/login', {
+      const response = await fetch('https://aayamregentbackend.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(loginData)
