0904ab4c494ed19f6f7b12c711354d64a53c3ba6
Vishal Kumar <calvk3@gmail.com>
2026-07-01 03:43:25 +0530

    SEO on work

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  
+  // 👇 Bas itna chahiye SPA routing ke liye
+  server: {
+    historyApiFallback: true
+  }
 })
